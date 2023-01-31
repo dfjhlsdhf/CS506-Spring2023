8d70c359eca8b835a6176e23459652b0a80ea2f9
dfjhlsdhf <qinchen4@outlook.com>
2023-01-31 16:02:59 -0500

    finish building hospital

--- labs/01-lab/infrastructure/hospital.py
+++ labs/01-lab/infrastructure/hospital.py
@@ -1,3 +1,8 @@
+from power import draw_power_plant
+from road import draw_road
+from tree import draw_tree
 def draw_hospital():
-    print("hospital not found")
+    draw_power_plant()
+    draw_road()
+    draw_tree()
     return
